ce8ac1bc9681618a2a52d654258e58f327013cbe
Flávio Souza <103860059+Flavio-Ad-Souza@users.noreply.github.com>
2022-09-15 23:36:41 -0300

    Upload do Projeto Alura Midi

---
+++ main.js
@@ -0,0 +1,64 @@
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+   // if (elemento != null && elemento.localName == 'audio') {
+    if (elemento && elemento.localName == 'audio') {
+        elemento.play();
+
+     } else{
+        // alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+     }        
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// Estrutura for (Para):
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];      
+    const idAudio = `#som_${instrumento}`; // Template String
+    
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }    
+
+    tecla.onkeydown = function(evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');          
+        }             
+    }
+    
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+}
+
+// Estrutura while (Enquanto):
+ /* 
+ let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];  
+
+    // Template String
+    const idAudio = `#som_${instrumento}`; 
+    // console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+    
+    contador = contador + 1;
+    // console.log(contador);    
+}
+*/
+
+
+
+
